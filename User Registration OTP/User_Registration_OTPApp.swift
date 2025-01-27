60b277217edf53512227f86d113750a23e204394
aaseem22 <aaseem.mhaskar.am@gmail.com>
2025-01-27 18:30:53 +0530

    Initial Commit

---
+++ User Registration OTP/User_Registration_OTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  User_Registration_OTPApp.swift
+//  User Registration OTP
+//
+//  Created by Aaseem Mhaskar on 27/01/25.
+//
+
+import SwiftUI
+
+@main
+struct User_Registration_OTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
